2184ff2a025040c7d544624a1fbd621caf4e9173
Iván Ares <ivarcass@gmail.com>
2023-09-26 21:43:36 +0200

    Edad y año nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,7 +3,8 @@ public class Prueba {
     public static void main(String args[]){
 
         System.out.println("Ares Casado");
-
+        System.out.println("Tengo 18 años y nací en 2005");
+        
     }
 
 }
