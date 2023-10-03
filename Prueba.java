231e850b65e76c84654a0c17e81c06abbbf29b91
Iván Ares <ivarcass@gmail.com>
2023-10-03 12:15:45 +0200

    Musica

--- Prueba.java
+++ Prueba.java
@@ -2,10 +2,9 @@ public class Prueba {
 
     public static void main(String args[]){
 
-        System.out.println("Ares Casado");
         System.out.println("Tengo 18 años y nací en 2005");
         System.out.println("Mi comida favorita son las hamburguesas");
-        
+        System.out.println("Mi grupo favorito es Coldplay");
     }
 
 }
